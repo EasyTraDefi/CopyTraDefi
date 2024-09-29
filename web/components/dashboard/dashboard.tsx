fe8b0adbea7e8dd2ea278ec6356a0edb7e3827ec
yubax <yassineelbachiri21@gmail.com>
2024-09-29 17:18:50 +0100

    chore : .env updated

--- web/components/dashboard/dashboard.tsx
+++ web/components/dashboard/dashboard.tsx
@@ -101,6 +101,20 @@ export function Dashboard({ }: DashboardProps) {
                         <SwapComponent />
                     </div>
                 </section>
+                <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="flex flex-col rounded-2xl w-96 bg-[#ffffff] shadow-xl">
+                        <figure className="flex justify-center items-center rounded-2xl">
+                            {/* <Image src="https://tailwind-generator.b-cdn.net/images/card-generator/tailwind-card-generator-card-preview.png" alt="Card Preview" class="rounded-t-2xl"> */}
+                        </figure>
+                        <div className="flex flex-col p-8">
+                            <div className="text-2xl font-bold   text-[#374151] pb-6">Generator</div>
+                            <div className=" text-lg   text-[#374151]">Leverage a graphical editor to create beautiful web components.</div>
+                            <div className="flex justify-end pt-6">
+                                <button className="bg-[#7e22ce] text-[#ffffff] w-full font-bold text-base  p-3 rounded-lg hover:bg-purple-800 active:scale-95 transition-transform transform">Try it out!</button>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </main>
         </div>
     );
