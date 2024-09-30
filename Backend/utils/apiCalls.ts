d67aec3c3ada09a094714ccd6f9d0bfd8872963a
yubax <yassineelbachiri21@gmail.com>
2024-09-30 21:24:30 +0100

    chore: apicalls cleaned

--- Backend/utils/apiCalls.ts
+++ Backend/utils/apiCalls.ts
@@ -77,36 +77,8 @@ export async function getNewTokens(): Promise<any[]> {
   return response.data;
 }
 
-// // utils/apiCalls.ts
 
-// import { Helius } from "helius-sdk";
-// import { TradeData } from '~/types/trade-data';
-// import { TransactionData } from '~/types/transaction-data';
 
-// const HELIUS_API_KEY = process.env.HELIUS_API as string;
 
-// const heliusClient = new Helius(HELIUS_API_KEY);
 
-// export const fetchTrades = async (): Promise<TradeData[]> => {
-//     const response = await fetch('/api/trades');
-//     return await response.json();
-// };
 
-// export const fetchTransactions = async (): Promise<TransactionData[]> => {
-//     const response = await fetch('/api/transactions');
-//     return await response.json();
-// };
-
-// export const executeTrade = async (trade: TradeData): Promise<string> => {
-//     try {
-//         // Implement the logic to execute the trade using Helius SDK
-//         // This is just a placeholder, you'll need to implement the actual execution logic
-//         console.log('Executing trade:', trade);
-
-//         // Simulating a successful transaction
-//         return '1234567890abcdef';
-//     } catch (error) {
-//         console.error('Error executing trade:', error);
-//         throw error;
-//     }
-// };
