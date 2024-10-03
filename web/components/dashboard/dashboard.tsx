2511ee9ec9adcae89b65e3b80a0cc61c7523be81
yubax <yassineelbachiri21@gmail.com>
2024-10-03 20:15:17 +0100

    chore: copy trader  updated

--- web/components/dashboard/dashboard.tsx
+++ web/components/dashboard/dashboard.tsx
@@ -64,7 +64,7 @@ export function Dashboard({ }: DashboardProps) {
         const dataToSend = {
             traderAddress,
             copyAmount: parseFloat(copyAmount),
-            symbol: 'BTC'
+            symbol: 'SOL'
         };
 
 
